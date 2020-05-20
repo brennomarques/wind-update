4c564195f8a08041c16ebed8d4ae1cb2a88ef935
Brenno marques <brenno.lima@soluti.com.br>
2020-05-20 09:29:06 -0300

    Commit inicial

---
+++ main.js
@@ -0,0 +1,42 @@
+
+const { app, BrowserWindow, dialog } = require('electron')
+const { autoUpdater } = require("electron-updater")
+const log = require('electron-log');
+
+autoUpdater.logger = log;
+autoUpdater.logger.transports.file.level = 'info';
+
+
+function createWindow() {
+    // Cria uma janela de navegação.
+    let win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    })
+
+    // e carregar o index.html do aplicativo.
+    win.loadFile('index.html')
+    autoUpdater.checkForUpdates();
+}
+// Inicio, Usando package.json para capturar nome e versão e colocar no Titulo
+
+app.whenReady().then(createWindow)
+
+autoUpdater.on('update-available', info => {
+    log.info('update-available');
+    const dialogOpts = {
+        type: 'info',
+        buttons: ['Atualizar', 'Agora Não'],
+        title: 'Atualização do Aplicativo',
+        message: 'Atualização Disponível',
+        detail: 'Uma nova versão do Assistente está disponível. É preciso reiniciar o aplicativo para aplicar as atualizações.'
+    }
+
+    dialog.showMessageBox(dialogOpts, (response) => {
+        if (response === 0) autoUpdater.quitAndInstall()
+    })
+
+});
